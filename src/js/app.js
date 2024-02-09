ae8b139c2fc9df1eede2b291ff60434e04d1fc22
Grokhen <152092079+Grokhen@users.noreply.github.com>
2024-02-09 20:02:03 +0000

    fix exercise

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,25 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+//EDIT ONLY INSIDE THIS RENDER FUNCTION
+//This function is called every time the user changes types or changes any input
+/* 
+ {
+  includeCover: true, // if includeCover is true the algorithm should show the cover image
+  background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+  avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+  socialMediaPosition: "right", // social media bar position (left or right)
 
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
+  twitter: null, // social media usernames
+  github: null,
+  linkedin: null,
+  instagram: null,
+
+  name: null,
+  lastName: null,
+  role: null,
+  country: null,
+  city: null
+};
  */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
@@ -28,19 +27,36 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  let name = variables.name ? variables.name : "Name";
+  let lastName = variables.lastName ? variables.lastName : "Last Name";
+  let role = variables.role ? variables.role : "role";
+  let city = variables.city ? variables.city : "City";
+  let country = variables.country ? variables.country : "Country";
+  let twitter = variables.twitter
+    ? variables.twitter
+    : "https://twitter.com/4geeksacademy";
+  let github = variables.github
+    ? variables.github
+    : "https://github.com/4geeksacademy";
+  let linkedin = variables.linkedin
+    ? variables.linkedin
+    : "https://linkedin.com/school/4geeksacademy";
+  let instagram = variables.instagram
+    ? variables.instagram
+    : "https://instagram.com/4geeksacademy";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="${linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -64,11 +80,11 @@ window.onload = function() {
     github: null,
     linkedin: null,
     instagram: null,
-    name: null,
-    lastName: null,
-    role: null,
-    country: null,
-    city: null
+    name: "Your Name",
+    lastName: "Your Lastname",
+    role: "role",
+    country: "country",
+    city: "city"
   };
   render(window.variables); // render the card for the first time
 
